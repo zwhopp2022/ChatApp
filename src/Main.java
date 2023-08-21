ecba8b83a7d0ef658418c4e4cb48e6a9cb6d5555
Zachary <zacharyhopp01@gmail.com>
2023-08-21 11:59:13 -0400

    added main boilerplate to project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Created by Zachary Hopp 8/21/2023
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Stay Tuned for more developments!");
+    }
+
+
+}
